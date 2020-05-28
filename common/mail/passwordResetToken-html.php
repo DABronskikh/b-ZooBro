7f7230fdd0ae4b9339e5847364178f5e28eb6061
bronskih <dima5202@mail.ru>
2020-05-28 19:37:09 +0500

    Update passwordResetToken-html.php

--- common/mail/passwordResetToken-html.php
+++ common/mail/passwordResetToken-html.php
@@ -4,10 +4,10 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $user common\models\User */
 
-$resetLink = Yii::$app->urlManager->createAbsoluteUrl(['reset-password', 'token' => $user->password_reset_token]);
+//$resetLink = Yii::$app->urlManager->createAbsoluteUrl(['reset-password', 'token' => $user->password_reset_token]);
+$resetLink = "https://zoobro.ru/?reset-password={$user->password_reset_token}";
 ?>
 <div class="password-reset">
     <p>Привет <?= Html::encode($user->email) ?>!</p>
-    <p>Для восстановления пароля перейдите по ссылке:</p>
-    <p><?= Html::a(Html::encode($resetLink), $resetLink) ?></p>
+    <p>Для восстановления пароля перейдите по <?= Html::a(Html::encode('ссылке'), $resetLink) ?></p>
 </div>
